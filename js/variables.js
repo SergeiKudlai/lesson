ec5f031c40de9fadc7e107e497fa0ddf59b8f48c
Sergei Kudlai <s.kudlai@mail.ru>
2023-09-17 12:51:09 +0300

    feat: added folder js and update files

---
+++ js/variables.js
@@ -0,0 +1,8 @@
+const coruselInner = document.querySelector(".corusel__inner");
+const BTN_LEFT = document.querySelector(".btn__left");
+const BTN_RIGHT = document.querySelector(".btn__right");
+const ITEM_LEFT = document.querySelector(".corusel__box--left");
+const ITEM_RIGHT = document.querySelector(".corusel__box--right");
+const slides = document.querySelectorAll(".slider__item");
+
+export { coruselInner, BTN_LEFT, BTN_RIGHT, ITEM_LEFT, ITEM_RIGHT, slides };
